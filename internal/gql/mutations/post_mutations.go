45bdf7536d1eaa8e66406dfd44eb6182724b980e
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2024-03-12 02:58:20 +0600

    Update post mutation

--- internal/gql/mutations/post_mutations.go
+++ internal/gql/mutations/post_mutations.go
@@ -1,6 +1,7 @@
 package mutations
 
 import (
+	"fmt"
 	"github.com/graphql-go/graphql"
 	"github.com/manjurulhoque/go-gql-crud/internal/gql/types"
 	"github.com/manjurulhoque/go-gql-crud/internal/models"
@@ -19,15 +20,47 @@ var PostMutations = graphql.Fields{
 			},
 		},
 		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-			newTodo := models.Post{
+			newPost := models.Post{
 				Title:       p.Args["title"].(string),
 				Description: p.Args["description"].(string),
 			}
-			err := dbc.GetDB().Create(&newTodo).Error
+			err := dbc.GetDB().Create(&newPost).Error
 			if err != nil {
 				return nil, err
 			}
-			return newTodo, nil
+			return newPost, nil
+		},
+	},
+	"updatePost": &graphql.Field{
+		Type: types.PostType,
+		Args: graphql.FieldConfigArgument{
+			"id": &graphql.ArgumentConfig{
+				Type: graphql.NewNonNull(graphql.Int),
+			},
+			"title": &graphql.ArgumentConfig{
+				Type: graphql.NewNonNull(graphql.String),
+			},
+			"description": &graphql.ArgumentConfig{
+				Type: graphql.NewNonNull(graphql.String),
+			},
+		},
+		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			id := p.Args["id"].(int)
+			post := models.Post{}
+			err := dbc.GetDB().First(&post, id).Error
+			if err != nil {
+				return nil, err
+			}
+			fmt.Println(p.Args)
+			title := p.Args["title"].(string)
+			description := p.Args["description"].(string)
+			post.Title = title
+			post.Description = description
+			err = dbc.GetDB().Save(post).Error
+			if err != nil {
+				return nil, err
+			}
+			return post, err
 		},
 	},
 }
